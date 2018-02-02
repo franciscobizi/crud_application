384a99ffe89f3f971966668edf5c1e9f12e86804
Francisco Bizi <taylorsoft28@gmail.com>
2018-02-02 21:01:33 +0200

    Added create, update and delete routes in route file

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -9,3 +9,7 @@
 $app->get('/', 'HomeController:index')->setName('index-page');
 $app->post('/signin', 'HomeController:signIn');
 $app->get('/user', 'HomeController:logged');
+
+$app->post('/update', 'EventCrontroller:updateEvent');
+$app->post('/delete', 'EventCrontroller:deleteEvent');
+$app->post('/create', 'EventCrontroller:createEvent');
